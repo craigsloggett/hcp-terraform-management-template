5f26678b87dd96e492746be1ad33379311b10f98
Craig Sloggett <craigsloggett@gmail.com>
2025-12-11 12:13:35 -0500

    feat: Add templated configuration

---
+++ imports.tf
@@ -0,0 +1,31 @@
+# The HCP Terraform organization.
+import {
+  id = module.discovery.tfe_organization.this.name
+  to = tfe_organization.this
+}
+
+# The members of the HCP Terraform organization.
+import {
+  for_each = module.discovery.tfe_organization_membership
+
+  id = each.key
+  to = tfe_organization_membership.this[each.key]
+}
+
+# The "owners" Team
+import {
+  id = "${module.discovery.tfe_organization.this.name}/${module.discovery.tfe_team.owners.id}"
+  to = tfe_team.owners
+}
+
+# The members of the "owners" team.
+import {
+  id = module.discovery.tfe_team.owners.id
+  to = tfe_team_organization_members.owners
+}
+
+# The "Default Project" Project
+import {
+  id = module.discovery.tfe_project.default.id
+  to = tfe_project.default
+}
--- versions.tf
+++ versions.tf
@@ -1,6 +1,12 @@
 terraform {
-  # Version v1.7.0 is the first version to introduce the `terraform test` command.
-  required_version = "~> 1.7"
+  # Version v1.6.0 is the first version to introduce the `terraform test` command.
+  # Version v1.5.0 is the first version to introduce `import` blocks.
+  required_version = "~> 1.5"
 
-  required_providers {}
+  required_providers {
+    tfe = {
+      source  = "hashicorp/tfe"
+      version = "0.71.0"
+    }
+  }
 }
