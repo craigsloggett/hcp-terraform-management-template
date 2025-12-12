fb14d43433e7bc6801cff3952dd922ef944d7d6e
Craig Sloggett <craigsloggett@gmail.com>
2025-12-12 18:27:34 -0500

    chore(deps): Bump discovery module version (#3)

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@
 # TFE_TOKEN environment variable in the Terraform run environment.
 module "discovery" {
   source  = "craigsloggett/discovery/tfe"
-  version = "0.14.0"
+  version = "0.14.2"
 }
 
 # The following are the resources that come with every new HCP Terraform organization.
